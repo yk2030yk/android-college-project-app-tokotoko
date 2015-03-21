7eb3a8f7e7800b7a6d2254f8d646a81df9faf0ab
yk2030yk <yk.ngsyk@gmail.com>
2015-03-21 20:19:15 +0900

    no message

---
+++ BitmapCache.java
@@ -0,0 +1,38 @@
+package com.example.chat;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+public class BitmapCache {
+        private LruCache<String, Bitmap> memoryCache;
+	 
+        public BitmapCache() {
+                int maxMemory = (int)(Runtime.getRuntime().maxMemory() / 1024);
+                int cacheSize = maxMemory / 8;
+ 
+                memoryCache = new LruCache<String, Bitmap>(cacheSize) {
+                        @Override
+                        protected int sizeOf(String key, Bitmap bitmap) {
+                        return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+                        }
+                };
+        }
+    
+        public Bitmap getBitmap(String url) {
+                return memoryCache.get(url);
+        }
+ 
+        public void putBitmap(String key, Bitmap bitmap) {
+                if (memoryCache.get(key) == null) {
+                        if (key != null && bitmap != null) {
+                                Bitmap old = memoryCache.put(key, bitmap);
+                                if (old != null){
+                                        if(!old.isRecycled()){
+                                                old.recycle();
+                                        }
+                                        old = null;
+                                }
+                        }
+                }
+        }
+}
